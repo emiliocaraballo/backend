58ad2fe23ee8d2400ac2a0df5cd56c3a1c64a581
Emilio Caraballo <emiliocaraballo9810@gmail.com>
2022-03-04 20:35:40 -0500

    v0.1.1.6

--- src/modules/profile/profile.joi.ts
+++ src/modules/profile/profile.joi.ts
@@ -1,22 +1,35 @@
 import * as Joi from 'joi';
 
-
 export const ProfileCreateJoi=Joi.object({
     name:Joi.string().required().min(1).max(255),
     description:Joi.string(),
     icon:Joi.string(),
     status:Joi.number().required(),
     users: Joi.object().required(),
-    menu: Joi.object().required()
+    menu:Joi.array()
+    .unique('sequence')
+    .items(
+        Joi.object()
+        .keys({
+        sequence: Joi.number().required()
+        }).required()
+    )
 });
 
 export const ProfileUpdateJoi=Joi.object({
     name:Joi.string().required().min(1).max(255),
     description:Joi.string(),
-    status:Joi.number().required(),
     icon:Joi.string(),
+    status:Joi.number().required(),
     users: Joi.object().required(),
-    menu: Joi.object().required()
+    menu:Joi.array()
+    .unique('sequence')
+    .items(
+        Joi.object()
+        .keys({
+        sequence: Joi.number().required()
+        }).required()
+    )
 });
 
 export const GetParamProfileJoi=Joi.object({
--- src/modules/profile/profile.repository.ts
+++ src/modules/profile/profile.repository.ts
@@ -4,30 +4,64 @@ import { getRepository } from 'typeorm';
 import { general } from 'src/config/general';
 import to from 'await-to-js';
 import { Profile } from 'src/database/entity/profile';
+import { MenuHasProfile } from 'src/database/entity/menusHasProfile';
+import {getConnection} from "typeorm";
 class ProfileRepository{
 
     public create=async (menu: IProfile): Promise<IQueryResponse> => {
 
-       const Query= getRepository(Profile).save(
-           {
-               createdAt: general.dateNow(),
-               updatedAt: general.dateNow(),
-               name: menu.name,
-               description: menu.description,
-               status: menu.status,
-               icon: menu.icon,
-               userCreated: menu.users.data.sequence,
-               userUpdated: menu.users.data.sequence
-           }
-       );
+        // transacion
 
-       const [error,result]= await to(Query);
-       if(!result){
+        
+        // get a connection and create a new query runner 
+        const connection = getConnection(); const queryRunner = connection.createQueryRunner(); 
+        // establish real database connection using our new query runner
+        await queryRunner.connect();
+        // lets now open a new transaction:
+        await queryRunner.startTransaction();
+
+        const profile=new Profile();
+        profile.createdAt=general.dateNow();
+        profile.updatedAt=general.dateNow();
+        profile.name=menu.name;
+        profile.description=menu.description;
+        profile.status=menu.status;
+        profile.icon=menu.icon;
+        profile.userCreated=menu.users.data.sequence;
+        profile.userUpdated=menu.users.data.sequence;
+
+        const [errorProfile,responseProfile]=await to(queryRunner.manager.save(profile));
+        console.log(errorProfile,responseProfile.sequence,"responseProfile");
+        if(!responseProfile){
             return {
                 statusCode:404,
                 message:"NOT_REQUEST"
             }
        }
+        
+
+        try {
+            
+            
+
+           
+           
+
+            // commit transaction now:
+            await queryRunner.commitTransaction();
+
+        } catch (error) {
+            // since we have errors let's rollback changes we made
+            await queryRunner.rollbackTransaction();
+        }finally {
+    
+            // you need to release query runner which is manually created:
+            await queryRunner.release();
+        }
+
+      
+       
+       // end:transacion
 
         return {
             statusCode:201
